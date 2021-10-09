d02f1e96a0088cba6237c49ec68dd263e52dc066
Gowtham Raj <gowthamrajskcet@gmail.com>
2021-10-09 20:52:33 +0530

    Update auth.module.ts

--- src/common/middlewares/auth/auth.module.ts
+++ src/common/middlewares/auth/auth.module.ts
@@ -5,8 +5,8 @@ import { JwtModule } from "@nestjs/jwt";
 @Module({
     imports: [
         JwtModule.register({
-            secret: process.env.JWT_SECRET,
-            signOptions: { expiresIn: process.env.JWT_EXPIRE_TIME },
+            secret: "Hell0W0rld@20@)",
+            signOptions: { expiresIn: "3600s" },
         }),
     ],
     providers: [AuthService],
